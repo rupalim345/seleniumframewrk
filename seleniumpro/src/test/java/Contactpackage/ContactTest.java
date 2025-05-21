8b25e364dcbc2519112620a7063327e3fc0adb7e
Rupali <anushruti.mohanty7@gmail.com>
2025-05-21 11:58:36 +0530

    push first

---
+++ seleniumpro/src/test/java/Contactpackage/ContactTest.java
@@ -0,0 +1,26 @@
+package Contactpackage;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+
+	@Test
+	public void test() {
+		
+		String URL=System.getProperty("url");
+		String BROWSER=System.getProperty("browser");
+		String USERNAME=System.getProperty("username");
+		String PASSWORD=System.getProperty("password");
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+		
+		System.out.println("create contacttest");
+	}
+	@Test
+	public void test1() {
+		System.out.println("fetch test1");
+	}
+}
---
+++ seleniumpro/src/test/java/orgpackage/OrgTest.java
@@ -0,0 +1,16 @@
+package orgpackage;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+
+	@Test
+	public void test1() {
+		System.out.println("create org test");
+	}
+	
+	@Test
+	public void test2() {
+		System.out.println("create test2");
+	}
+}
